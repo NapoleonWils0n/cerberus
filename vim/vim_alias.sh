90b81fc33743333f413c49851f0f2029f76beb88
Daniel J Wilcox <danieljwilcox@gmail.com>
2012-11-23 17:45:22 +0000

    vim bash alias

---
+++ vim/vim_alias.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+alias v="vim ."
+
+# v
+
+# A directory listing will show up. Now you can do the following:
+
+# Use j/k to go up & down
+# Place cursor on a dir & press enter to explore it
+# Place cursor on a file & press enter to edit it
+# Use - to go up a dir
+# Use D to delete a file or dir
+# Use R to rename a file or dir
+# Use s to change the sorting of items (name, time, size)
+# Use d to create a dir
+# Use i to cycle between listing formats (thin, long, wide, tree)
+# Use Ctrl-l to refresh a directory listing
+# You can do a lot more with netrw too! It can be used for remote file browsing over ssh. For more info, check the help file with F1.
+
+
