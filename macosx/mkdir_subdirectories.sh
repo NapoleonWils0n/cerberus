67f815a4283ca364047c4cf253cd4e0bd8751f4f
Daniel J Wilcox <danieljwilcox@gmail.com>
2012-12-22 16:07:06 +0000

    create multiple files or directories

---
+++ macosx/mkdir_subdirectories.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# mkdir create multiple directories
+mkdir -p playground/dir-{00{1..9},0{10..99},100}
+
+# touch create multiple files
+touch playground/dir-{00{1..9},0{10..99},100}/file-{A..Z}
