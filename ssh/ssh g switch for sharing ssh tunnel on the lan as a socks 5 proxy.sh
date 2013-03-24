959a2d2db4f154e4520cba422162fa280b546e63
Daniel J Wilcox <danieljwilcox@gmail.com>
2013-03-24 18:08:40 +0000

    ssh -g switch for sharing connection on lan

---
+++ ssh/ssh g switch for sharing ssh tunnel on the lan as a socks 5 proxy.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# ssh g switch for sharing ssh tunnel on the lan as a socks 5 proxy
+ssh -g -D 8001 -p 443 username@sshserver.com
+
+# -g = share ssh tunnel on lan
+# -D 8001 = localport
+# -p 443 = remote ssh port
+
+
+# search for ssh process id, if you want to kill the connection
+ps aux | grep "[s]sh -D"
+
+
+# You can now connect to the ssh tunnel on the lan as a socks 5 proxy
+
+# Change your network setting to connect to the ip address and port of the computer running the ssh tunnel
+
+# socks proxy 
+# ip: 10.0.0.1
+# port: 8001
