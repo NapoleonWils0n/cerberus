dd31760e4db40cd43ffc005b6f707cb921fc19ea
Daniel J Wilcox <danieljwilcox@gmail.com>
2013-03-25 22:10:42 +0000

    raspberry pi backup sdcard

--- raspberry pi/raspberry_pi_router/raspberry_pi_3g_router.sh
+++ raspberry pi/raspberry_pi_router/raspberry_pi_3g_router.sh
@@ -809,3 +809,19 @@ sudo service hostapd start
 sudo service hostapd stop
 
 
+#|------------------------------------------------------------------------------
+#|	raspberry pi backup sdcard
+#|------------------------------------------------------------------------------
+
+# plug in the sdcard and wait for it mount
+# find the disk has been called by running the df -h command
+df -h
+
+# the disk should be labeled something like /dev/disk2s1
+# you need to unmount the disk before you can copy it
+
+# unmount the disk
+diskutil unmountDisk /dev/disk2s1
+
+# backup the sdcard wth dd
+sudo dd bs=1m if=/dev/disk2s1 of=/Users/$USER/Desktop/raspberry_pi_router.img
