819a153879c4e893290259743766a0e4996d1d2e
Daniel J Wilcox <danieljwilcox@gmail.com>
2013-04-04 23:24:01 +0100

    raspbmc usb dac

---
+++ raspberry pi/raspbmc/raspbmc_usb_dac.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# raspbmc usb dac
+
+
+# ssh into raspbmc and run alsamixer to slect the sound card
+alsamixer
+
+# press f6 to select the sound card and then escape to exit
+
+# on the raspbmc go to programs raspbmc
+
+# then select the system configuration tab and slect audio engine
+# click ok and reboot
+
+# the audio should now conme out the usb dac and not over hdmi
+
