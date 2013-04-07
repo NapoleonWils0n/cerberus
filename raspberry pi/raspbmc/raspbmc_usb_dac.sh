df1ace161daf8b1836e36dcd041fd6b3861d7bfc
Daniel J Wilcox <danieljwilcox@gmail.com>
2013-04-07 03:07:18 +0100

    fix audio sync with usb dac

--- raspberry pi/raspbmc/raspbmc_usb_dac.sh
+++ raspberry pi/raspbmc/raspbmc_usb_dac.sh
@@ -17,3 +17,38 @@ alsamixer
 
 # the audio should now conme out the usb dac and not over hdmi
 
+ssh into rapsbmc
+
+
+cd .xbmc/userdata/
+
+# create the advancedsettings.xml and paste in the xml below
+
+sudo nano advancedsettings.xml
+
+
+<advancedsettings>
+  <video>
+    <latency>
+      <delay>250</delay>
+      <refresh>
+        <min>23</min>
+        <max>24</max>
+        <delay>175</delay>
+      </refresh>
+    </latency>
+  </video>
+</advancedsettings>
+
+
+# change video playback av sync
+
+# go to following menu
+# system/video settings/playback
+
+# change av sync method to video clock (resample audio)
+
+# sync playback to display
+
+# av sync method: video clock (resample audio)
+
