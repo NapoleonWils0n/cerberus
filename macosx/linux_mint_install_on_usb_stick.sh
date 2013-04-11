ca2b761640ed8bd4ed89797855b455a0a7fc26b0
Daniel J Wilcox <danieljwilcox@gmail.com>
2013-04-11 16:22:43 +0100

    linux mint install on usb drive for mac

---
+++ macosx/linux_mint_install_on_usb_stick.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# linux mint install on usb stick
+
+
+# Boot your system using the Linux Mint 14 live CD or USB stick
+
+# enable LUKS full disk encryption
+sudo apt-get remove ubiquity
+sudo apt-get update
+sudo apt-get install ubiquity
+sudo ubiquity
