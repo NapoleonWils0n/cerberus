323777c76f1476624276956dd4cfbc5fc34cc767
Daniel J Wilcox <djwilcox@anubis.(none)>
2013-04-25 01:27:14 +0100

    clamav install and setup

---
+++ linux_mint/clamav_install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# install clamav
+
+sudo apt-get install clamav clamav-daemon clamav-freshclam clamtk
+
+# update freshclam
+sudo freshclam
+
+# start clamav daemon
+sudo /etc/init.d/clamav-daemon start
