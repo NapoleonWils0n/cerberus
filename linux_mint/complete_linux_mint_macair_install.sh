87b44dec34bb09dc1d078b93848fd8dea3eb1937
Daniel J Wilcox <danieljwilcox@gmail.com>
2013-05-05 23:24:08 +0100

    psad noemail and dotfiles set up

--- linux_mint/complete_linux_mint_macair_install.sh
+++ linux_mint/complete_linux_mint_macair_install.sh
@@ -247,6 +247,12 @@ sudo apt-get install psad
 # press enter during the install to accept the default options
 
 
+# edit psad.conf and change the ALERTING_METHODS to noemail
+sudo vim /etc/psad/psad.conf
+
+ALERTING_METHODS            noemail;
+
+
 #|------------------------------------------------------------------------------
 #|	xbmc install
 #|------------------------------------------------------------------------------
@@ -573,4 +579,51 @@ sudo apt-get install nemo-dropbox
 # truecrypt
 # download the 64 bit tar.gz file from truecrypt
 # untar the file and run the script
+
+#|------------------------------------------------------------------------------
+#| dotfiles
+#|------------------------------------------------------------------------------
+
+# create .dotfiles in home directory if it doesnt already exist
+mkdir -p ~/.dotfiles
+
+# files to move to .dotfiles
+mv ~/.bashrc ~/.dotfiles
+mv ~/.gitconfig ~/.dotfiles
+mv ~/.git-prompt.sh ~/.dotfiles
+mv ~/.inputrc ~/.dotfiles
+mv ~/.profile ~/.dotfiles
+mv ~/.vimrc ~/.dotfiles
+mv ~/.vim ~/.dotfiles
+mv ~/.fonts ~/.dotfiles
+
+
+# create the symlinks
+ln -s ~/.dotfiles/.bashrc ~/.bashrc
+ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/.git-prompt.sh ~/.git-prompt.sh
+ln -s ~/.dotfiles/.inputrc ~/.inputrc
+ln -s ~/.dotfiles/.profile ~/.profile
+ln -s ~/.dotfiles/.vimrc ~/.vimrc
+ln -s ~/.dotfiles/.vim ~/.vim
+ln -s ~/.dotfiles/.fonts ~/.fonts
+
+# create the git repo in dotfiles
+cd ~/.dotfiles
+
+# login to github and create a new git dotfiles repo
+
+# git init repo
+git init
+
+# add all the dotfiles
+git add .
+
+# add the remote origin
+git remote add origin git@github.com:username/dotfiles.git
+
+# push origin master
+git push -u origin master
+
+
 
