62707736823b323466839c19ee9dfbc2bfb7914d
Daniel J Wilcox <danieljwilcox@gmail.com>
2013-05-11 16:18:50 +0100

    double click windows to toggle maximize

--- linux_mint/complete_linux_mint_macair_install.sh
+++ linux_mint/complete_linux_mint_macair_install.sh
@@ -963,21 +963,3 @@ apt-get install glibc-doc manpages-dev libc6-dev gcc build-essential
 # between normal size and fullscreen the text will jump to accomadte the menu bar
 #
 # you can show the menu bar again by clicking with 2 fingers on the trackpad and selecting show menubar or by right clicking in the terminal window
-
-
-#|------------------------------------------------------------------------------
-#| double click to minimize windows
-#|------------------------------------------------------------------------------
-
-# open cinnamon settings
-
-# then open the windows tab
-
-# change Action on title bar double click to Minimize
-
-
-
-
-
-
-
