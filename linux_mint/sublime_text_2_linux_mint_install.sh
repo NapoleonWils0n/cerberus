db4ea6de808b945b726da382486f8c21c557ddf7
Daniel J Wilcox <danieljwilcox@gmail.com>
2013-05-11 17:22:51 +0100

    sublime text 2 install

---
+++ linux_mint/sublime_text_2_linux_mint_install.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+# sublime text 2 linux mint install
+
+
+# download the 64bit tar.gz sublime text 2
+
+# untar the file
+cd Downloads
+tar xf Sublime\ Text\ 2.0.1\ x64.tar.bz2
+
+
+# You’ll get a “Sublime Text 2″ folder after extraction. 
+# This folder contains all the files that Sublime Text will need. 
+# So we have to move that folder somewhere more appropriate. Like the “/opt/” folder :
+sudo mv Sublime\ Text\ 2 /opt/
+
+
+# Create a symbolic link in “/usr/bin”
+sudo ln -s /opt/Sublime\ Text\ 2/sublime_text /usr/bin/sublime
+
+# Create a desktop launcher. 
+# To do this, we’re going to create a .desktop file in “/usr/share/applications”:
+sudo sublime /usr/share/applications/sublime.desktop
+
+# paste in the following content
+
+[Desktop Entry]
+Encoding=UTF-8
+Name=Sublime Text
+Comment=Sublime Text 2
+Exec=sublime
+Icon=/opt/Sublime Text 2/Icon/48x48/sublime_text.png
+Terminal=false
+Type=Application
+Categories=GNOME;GTK;Utility;TextEditor;
+StartupNotify=true
+
+# Now you would probably want to open all text files with Sublime Text 2. 
+# The easiest way to do that is to open up the file associations list:
+
+gksu sublime /usr/share/applications/defaults.list
+
+# And replace all occurrences of gedit.desktop with sublime.desktop.
