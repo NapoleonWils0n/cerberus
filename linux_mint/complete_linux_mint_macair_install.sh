4eb9b6844ee48d095d7d6cb5213ed64061929ece
Daniel J Wilcox <danieljwilcox@gmail.com>
2013-05-15 02:09:19 +0100

    install google command line tools

--- linux_mint/complete_linux_mint_macair_install.sh
+++ linux_mint/complete_linux_mint_macair_install.sh
@@ -1030,4 +1030,42 @@ StartupNotify=true
 
 # install sublime linter 
 
-# install git package
+
+#|------------------------------------------------------------------------------
+#| install google command line tools
+#|------------------------------------------------------------------------------
+
+sudo apt-get install googlecl
+
+
+# conect the google command line tool to your google account
+
+# run google and try to list one of your contacts ( replace name with a name in your contacts )
+google contact list name
+
+# you will be prompted to enter you gmail address
+# enter your gmail address and press enter
+# your web browser will open and you have to log into your gmail account
+
+# you will then be asked to grant access for googlecl 
+# you will then be given a verification code you need to paste into the terminal
+
+# paste in the verification code and press return
+
+# this will create a config file in "/home/username/.config/googlecl/config"
+
+# edit the config file and add phone to the fields under contacts
+# so you can list phone numbers 
+
+# change from this 
+
+# [CONTACTS]
+# fields = name,email
+
+# to this
+
+# [CONTACTS]
+# fields = name,email,phone
+
+
+
