25948e7e20e6901ceeb7b6ef9e70c4fbcf4bd757
Daniel J Wilcox <danieljwilcox@gmail.com>
2013-05-26 01:11:35 +0100

    guest additions install

--- linux_mint/virtualbox-install.sh
+++ linux_mint/virtualbox-install.sh
@@ -11,3 +11,21 @@ sudo usermod -a -G vboxusers username
 # https://www.virtualbox.org/wiki/Downloads
 # double click the download file to install the extension pack
 
+# in the guest download the latest guest additions
+# http://download.virtualbox.org/virtualbox/4.2.8/VBoxGuestAdditions_4.2.8.iso
+
+# mount the iso
+mount VBoxGuestAdditions_4.2.8.iso /media/cdrom
+
+# copy the VBoxLinuxAdditions.run to /root/
+cp /media/cdrom/VBoxLinuxAdditions.run /root/
+
+# make VBoxLinuxAdditions.run executable
+cd /root/
+chmod +x VBoxLinuxAdditions.run 
+
+# now run the script
+./VBoxLinuxAdditions.run 
+
+# reboot and you should be able to go fullscreen
+reboot
