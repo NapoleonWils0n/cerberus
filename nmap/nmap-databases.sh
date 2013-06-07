2b3b7b2f70bf07a1621b9ee7ec71bf89eeb1cd36
Daniel J Wilcox <danieljwilcox@gmail.com>
2013-06-07 18:33:46 +0100

    nmap scan database

---
+++ nmap/nmap-databases.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Discovering Databases
+# Discovering SQL Server default TCP Port:
+
+Nmap –sP –p 1433 x.x.x.x/x
+
+# Discovering SQL Server default UDP Port:
+Nmap –sU –p 1434 x.x.x.x/x
+
+# Discovering Oracle Server default TCP Port:
+Nmap –sP –p 1521 x.x.x.x/x
