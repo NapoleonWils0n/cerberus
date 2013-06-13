b2bbee751f5b36d88f63f4c33417b0963f4f7c94
Daniel J Wilcox <danieljwilcox@gmail.com>
2013-06-13 20:53:23 +0100

    screen commands

---
+++ screen/screen.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# screen
+
+# start a new screen session with session name
+screen -S name
+
+# attach to a running session with name
+screen -r name
+
+# exit screen
+Control-a d
+
+# detach and logout (quick exit): C-a D D
+Control-a D D 
+
+# Help
+Control-a ?
