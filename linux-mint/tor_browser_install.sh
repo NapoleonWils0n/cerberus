7ac6c861a3b4323c9acbfb5c08f508f6f6fe1b51
Daniel J Wilcox <danieljwilcox@gmail.com>
2013-06-14 23:37:02 +0100

    update tor browser from ppa

--- linux-mint/tor_browser_install.sh
+++ linux-mint/tor_browser_install.sh
@@ -6,3 +6,14 @@ sudo add-apt-repository ppa:upubuntu-com/tor64
 sudo apt-get update
 sudo apt-get install tor-browser
 sudo chown $USER -R ~/.tor-browser/
+
+# update tor-browser from ppa
+
+# purge tor-browser
+sudo apt-get purge tor-browser
+
+# reinstall tor-browser
+sudo apt-get install tor-browser
+
+# change permissions on ~/.tor-browser
+chown -r $USER:$USER ~/.tor-browser
