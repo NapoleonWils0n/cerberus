f221ed367c591f0ee7294b35c7e8939e4fd288cb
Daniel J Wilcox <danieljwilcox@gmail.com>
2013-06-29 18:44:59 +0100

    convert html to pdf

---
+++ wkhtmltopdf/wkhtmltopdf-install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# wkhtmltopdf install
+
+# install dependencys
+sudo apt-get install openssl build-essential xorg libssl-dev libxrender-dev
+sudo apt-get install libqt4-dev qt4-dev-tools
+
+sudo apt-get install wkhtmltopdf
