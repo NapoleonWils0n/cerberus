24ba3a187fe6a323e526f4e18693dd1a678a7e33
Daniel J Wilcox <danieljwilcox@gmail.com>
2013-07-02 23:56:03 +0100

    perl hello world

---
+++ perl/perl-command-line.pl
@@ -0,0 +1,7 @@
+#!/usr/bin/perl
+
+# run perl from the command line
+perl -e 'print "hello world\n";'
+
+# -e option, the next argument is a perl statement
+# enclose the command in single quotes ''
