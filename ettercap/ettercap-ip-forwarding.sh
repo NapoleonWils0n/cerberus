c39768dd158dcab19b22d7cf6f826b2f9813c17b
Daniel J Wilcox <danieljwilcox@gmail.com>
2013-07-14 00:21:45 +0100

    ettercap ipforwarding

---
+++ ettercap/ettercap-ip-forwarding.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# First thing we need to do is to enable packet forwarding.
+
+# In the file /etc/sysctl.conf, we need to uncomment the following line (should be line 28).
+net.ipv4.ip_forward=1
+
+# uncomment iptables in /etc/etter.conf
+vim /etc/etter.conf
+
