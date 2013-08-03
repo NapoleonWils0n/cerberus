923b68975d95f2e383409c43fd8be7b223366dfd
Daniel J Wilcox <danieljwilcox@gmail.com>
2013-08-04 00:20:53 +0100

    ffmpeg hls streaming to mediastream segmenter

---
+++ ffmpeg/ffmpeg-hls-streaming.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# encapsulate an h264 acc video in a mpegts container
+# and the stream it via udp to a mac and open the stream with the mediasteam segmenter for hls streaming
+
+ffmpeg -re -i video.mp4 -vcodec copy -acodec copy -vbsf h264_mp4toannexb -f mpegts udp://192.168.1.7:1234?pkt_size=1316
