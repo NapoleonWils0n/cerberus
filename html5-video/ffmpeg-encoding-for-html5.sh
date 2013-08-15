f55491f243720123c0582996460a02250535f47e
Daniel J Wilcox <danieljwilcox@gmail.com>
2013-08-15 17:21:36 +0100

    ffmpeg html5 video

---
+++ html5-video/ffmpeg-encoding-for-html5.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+#==========================================================#
+# 720p = 1280x720 - high profile - level 4.1
+#==========================================================#
+
+# 1st pass
+#=========
+
+ffmpeg -i infile.mp4 \
+-c:v libx264 -profile:v high -level:v 4.1 -preset slow \
+-b:v 4750k -bufsize 1835k \
+-vf "yadif=0:-1:0, scale=1280:trunc(ow/a/2)*2" \
+-threads 0 -x264opts keyint=250:min-keyint=25 \
+-pass 1 -an \
+-f mp4 720p-high.mp4
+
+
+# 2nd pass
+#=========
+
+ffmpeg -i infile.mp4 \
+-c:v libx264 -profile:v high -level:v 4.1 -preset slow \
+-b:v 4750k -bufsize 1835k \
+-vf "yadif=0:-1:0, scale=1280:trunc(ow/a/2)*2" \
+-threads 0 -x264opts keyint=250:min-keyint=25 \
+-pass 2 -c:a libfdk_aac -b:a 192k \
+-f mp4 720p-high.mp4
+
+#==========================================================#
+# 480p = 720x480p - main profile - level 3.1
+#==========================================================#
+
+# 1st pass
+#=========
+
+ffmpeg -i infile.mp4 \
+-c:v libx264 -profile:v main -level:v 3.1 -preset slow \
+-b:v 2500k -bufsize 1835k \
+-vf "yadif=0:-1:0, scale=720:trunc(ow/a/2)*2" \
+-threads 0 -x264opts keyint=250:min-keyint=25 \
+-pass 1 -an \
+-f mp4 480p-main.mp4
+
+
+# 2nd pass
+#=========
+
+ffmpeg -i infile.mp4 \
+-c:v libx264 -profile:v main -level:v 3.1 -preset slow \
+-b:v 2500k -bufsize 1835k \
+-vf "yadif=0:-1:0, scale=720:trunc(ow/a/2)*2" \
+-threads 0 -x264opts keyint=250:min-keyint=25 \
+-pass 2 -c:a libfdk_aac -b:a 128k \
+-f mp4 480p-main.mp4
+
+#==========================================================#
+# qt-faststart - add moov atom to front of file
+#==========================================================#
+
+qt-faststart infile.mp4 outfile.mp4
+
---
+++ html5-video/html/video-resize.js
@@ -0,0 +1,17 @@
+function myAddListener(){
+	 var myVideo = document.getElementsByTagName('video')[0]; 
+         myVideo.addEventListener('loadedmetadata', removePoster, false);        	
+	 myVideo.addEventListener('loadedmetadata', naturalSize, false);
+	 myVideo.preload="none";
+	 myVideo.autoplay="none";
+}
+function removePoster() {
+         var myVideo = document.getElementsByTagName('video')[0];
+	 myVideo.poster='';
+	 myVideo.play();
+}
+function naturalSize() {
+         var myVideo = document.getElementsByTagName('video')[0];
+         myVideo.height = myVideo.videoHeight;
+         myVideo.width = myVideo.videoWidth;
+}
