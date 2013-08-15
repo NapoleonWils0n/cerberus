7b849cad8a92aa47b945884abcf5432b6ce0c2d0
Daniel J Wilcox <danieljwilcox@gmail.com>
2013-08-15 22:46:25 +0100

    basline profile for android

--- html5-video/ffmpeg-encoding-for-html5.sh
+++ html5-video/ffmpeg-encoding-for-html5.sh
@@ -54,6 +54,34 @@ ffmpeg -i infile.mp4 \
 -pass 2 -c:a libfdk_aac -b:a 128k \
 -f mp4 480p-main.mp4
 
+
+#==========================================================#
+# 480x360 - baseline profile - level 1.3 for Android
+#==========================================================#
+
+# 1st pass
+#=========
+
+ffmpeg -i infile.mp4 \
+-c:v libx264 -profile:v baseline -level:v 1.3 -preset slow \
+-b:v 450k -bufsize 1835k \
+-vf "yadif=0:-1:0, scale=480:trunc(ow/a/2)*2" \
+-threads 0 -x264opts keyint=250:min-keyint=25 \
+-pass 1 -an \
+-f mp4 360-baseline.mp4
+
+
+# 2nd pass
+#=========
+
+ffmpeg -i infile.mp4 \
+-c:v libx264 -profile:v baseline -level:v 1.3 -preset slow \
+-b:v 450k -bufsize 1835k \
+-vf "yadif=0:-1:0, scale=480:trunc(ow/a/2)*2" \
+-threads 0 -x264opts keyint=250:min-keyint=25 \
+-pass 2 -c:a libfdk_aac -b:a 128k \
+-f mp4 360-baseline.mp4
+
 #==========================================================#
 # qt-faststart - add moov atom to front of file
 #==========================================================#
