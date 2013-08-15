40901efd320f01256a1b464a6c4b7b4f67151906
Daniel J Wilcox <danieljwilcox@gmail.com>
2013-08-15 02:03:45 +0100

    global headers for hls android

--- http-live-streaming/ffmpeg-encoding-for-hls.sh
+++ http-live-streaming/ffmpeg-encoding-for-hls.sh
@@ -15,9 +15,10 @@ ffmpeg -i input.avi -ss 00:00:00 -t 00:05:00 -async 1 -vcodec copy -acodec copy 
 
 ffmpeg -i infile.mp4 \
 -c:v libx264 -profile:v high -level:v 4.1 -preset slow \
--b:v 4750k -bufsize 1835k \
+-bsf:v h264_mp4toannexb -b:v 4750k -bufsize 1835k \
 -vf "yadif=0:-1:0, scale=1280:trunc(ow/a/2)*2" \
--threads 0 -x264opts keyint=250:min-keyint=25 \
+-threads 0 -pix_fmt yuv420p -flags -global_header \
+-x264opts keyint=250:min-keyint=25 \
 -pass 1 -an \
 -f mp4 720p-high.mp4
 
@@ -27,9 +28,10 @@ ffmpeg -i infile.mp4 \
 
 ffmpeg -i infile.mp4 \
 -c:v libx264 -profile:v high -level:v 4.1 -preset slow \
--b:v 4750k -bufsize 1835k \
+-bsf:v h264_mp4toannexb -b:v 4750k -bufsize 1835k \
 -vf "yadif=0:-1:0, scale=1280:trunc(ow/a/2)*2" \
--threads 0 -x264opts keyint=250:min-keyint=25 \
+-threads 0 -pix_fmt yuv420p -flags -global_header \
+-x264opts keyint=250:min-keyint=25 \
 -pass 2 -c:a libfdk_aac -b:a 192k \
 -f mp4 720p-high.mp4
 
@@ -42,9 +44,10 @@ ffmpeg -i infile.mp4 \
 
 ffmpeg -i infile.mp4 \
 -c:v libx264 -profile:v main -level:v 3.1 -preset slow \
--b:v 2500k -bufsize 1835k \
+-bsf:v h264_mp4toannexb -b:v 2500k -bufsize 1835k \
 -vf "yadif=0:-1:0, scale=720:trunc(ow/a/2)*2" \
--threads 0 -x264opts keyint=250:min-keyint=25 \
+-threads 0 -pix_fmt yuv420p -flags -global_header \
+-x264opts keyint=250:min-keyint=25 \
 -pass 1 -an \
 -f mp4 480p-main.mp4
 
@@ -54,9 +57,10 @@ ffmpeg -i infile.mp4 \
 
 ffmpeg -i infile.mp4 \
 -c:v libx264 -profile:v main -level:v 3.1 -preset slow \
--b:v 2500k -bufsize 1835k \
+-bsf:v h264_mp4toannexb -b:v 2500k -bufsize 1835k \
 -vf "yadif=0:-1:0, scale=720:trunc(ow/a/2)*2" \
--threads 0 -x264opts keyint=250:min-keyint=25 \
+-threads 0 -pix_fmt yuv420p -flags -global_header \
+-x264opts keyint=250:min-keyint=25 \
 -pass 2 -c:a libfdk_aac -b:a 128k \
 -f mp4 480p-main.mp4
 
