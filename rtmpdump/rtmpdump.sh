062d734b4dd2c2d7b0395889fd3feb594afab773
Daniel J Wilcox <danieljwilcox@gmail.com>
2013-08-20 15:48:39 +0100

    iptables rules for rtmpdump

--- rtmpdump/rtmpdump.sh
+++ rtmpdump/rtmpdump.sh
@@ -2,13 +2,17 @@
 
 # rtmpdump install
 # ================
+
 sudo apt-get install rtmpdump
 
+
 # configure iptables to redirect rtmp traffic through local port
-sudo iptables -t nat -A OUTPUT -p tcp --dport 1935 - REDIRECT
+#===============================================================
 
-# show iptables rules by line number
-sudo iptables -L INPUT --line-numbers
+iptables -t nat -A OUTPUT -p tcp --dport 1935 -m owner \! --uid-owner root -j REDIRECT
 
-# delete iptables rule by line number
-sudo iptables -D INPUT 2
+
+# remove iptables rtmp redirect
+# =============================
+
+iptables -t nat -A OUTPUT -p tcp --dport 1935 -m owner \! --uid-owner root -j REDIRECT
