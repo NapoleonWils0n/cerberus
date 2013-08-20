a29ee99eed5ac7fe754da513e79a635dfd280747
Daniel J Wilcox <danieljwilcox@gmail.com>
2013-08-20 16:01:59 +0100

    rtmpsuck commands

--- rtmpdump/rtmpdump.sh
+++ rtmpdump/rtmpdump.sh
@@ -12,7 +12,31 @@ sudo apt-get install rtmpdump
 iptables -t nat -A OUTPUT -p tcp --dport 1935 -m owner \! --uid-owner root -j REDIRECT
 
 
+# run rtmpsuck as root
+# ====================
+
+su -ml
+rtmpsuck
+
+# open the video stream in a browser
+# ==================================
+
+# now open the stream in a browser and rtmpsuck should capture the urls
+
+# press control c to stop rtmpsuck
+
+# rtmpsuck will give you the command for rtmpdump to dump the video stream
+
+# before you run the rtmpdump command you need to remove the iptables rules
+
+
 # remove iptables rtmp redirect
 # =============================
 
 iptables -t nat -A OUTPUT -p tcp --dport 1935 -m owner \! --uid-owner root -j REDIRECT
+
+
+# run the rtmpdump command from rtmpsuck
+#=======================================
+
+# you can pipe the stream into mplayer by removing -o outfile.flv and replacing it with | mplayer -
