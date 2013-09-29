baf95ad0eedd76ee01984626c3703637c2baff98
Daniel J Wilcox <danieljwilcox@gmail.com>
2013-09-29 18:04:16 +0100

    fixed word wrap

--- linux-mint/xfce-linux-mint-macair-install.sh
+++ linux-mint/xfce-linux-mint-macair-install.sh
@@ -624,7 +624,8 @@ widget_class "*XfdesktopIconView*" style "xfdesktop-icon-view"
 # xfce4 terminal with drop down 
 #==============================
 
-# You can assign a keyboard shortcut for summoning the terminal (Settings Manager > Keyboard > Application Keyboard Shortcuts). 
+# You can assign a keyboard shortcut for summoning the terminal
+# (Settings Manager > Keyboard > Application Keyboard Shortcuts). 
 
 # create a new new keyboard and paste in this command
 xfce4-terminal --drop-down
@@ -632,7 +633,8 @@ xfce4-terminal --drop-down
 # then set the keyboard hotkey
 # eg: cmd + space
 
-# open the drop down terminal with the hotkey and then right click on the terminal and select preferences
+# open the drop down terminal with the hotkey 
+# and then right click on the terminal and select preferences
 
 # set the height, width and position for the terminal
 # deselect always show tabs
