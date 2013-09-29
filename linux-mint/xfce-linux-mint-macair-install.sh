794d732512aca6afc79d048028b0bb466cad3a0a
Daniel J Wilcox <danieljwilcox@gmail.com>
2013-09-29 18:56:39 +0100

    bittorent sync install

--- linux-mint/xfce-linux-mint-macair-install.sh
+++ linux-mint/xfce-linux-mint-macair-install.sh
@@ -950,6 +950,31 @@ libmp3lame-dev libopus-dev libsdl1.2-dev libtheora-dev libtool libva-dev libvdpa
 libvorbis-dev libvpx-dev libx11-dev libxext-dev libxfixes-dev texi2html zlib1g-dev
 hash -r
 
+# bittorent sync linux install
+#=============================
+
+# download the 64 bit version of btsync for linux
+
+# untar the file
+tar -zxvf btsync_x64.tar.gz
+
+# remove the tar file
+rm btsync_x64.tar.gz
+
+# move the btsync binary to your bin directory
+mv btsync ~/bin
+
+# make the binary executable
+chmod +x ~/bin/btsync
+
+# save the config file in the btsync ~/.sync directory
+btsync --dump-sample-config > ~/.sync
+
+# edit your ~/.bashrc
+vim ~/.bashrc
+
+# add and alias so btsync starts up with the config file
+alias btsync='btsync --config ~/.sync'
 
 # gitignore_global for bittorent sync
 #====================================
