353bd6a65f332272c6332c7e92981ffeaa859adb
Daniel J Wilcox <danieljwilcox@gmail.com>
2013-10-14 13:38:58 +0100

    chromium desktop launcher with no referrers

--- linux-mint/xfce-linux-mint-macair-install.sh
+++ linux-mint/xfce-linux-mint-macair-install.sh
@@ -1119,3 +1119,21 @@ sudo restart network-manager
 # ============================================
 
 # https://www.opendns.com/welcome/
+
+
+
+
+# dont send referrer headers with chrome
+# ======================================
+
+# start chromium with the no reffers switch
+# =========================================
+
+chromium-browser --no-referrers
+
+
+# create a bash alias in .bashrc 
+# ==============================
+
+# dont send referrer headers with chrome
+alias chromium-browser='chromium-browser --no-referrers'
