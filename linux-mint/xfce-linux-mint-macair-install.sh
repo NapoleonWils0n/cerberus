a3918353819bf0cf442ece1d5420fd83cda375b6
Daniel J Wilcox <danieljwilcox@gmail.com>
2013-10-21 20:23:31 +0100

    headphones usenet audio client

--- linux-mint/xfce-linux-mint-macair-install.sh
+++ linux-mint/xfce-linux-mint-macair-install.sh
@@ -1224,3 +1224,31 @@ sudo vim /etc/default/couchpotato
 
 # replace "username" with your username
 CP_USER=username
+
+
+# headphones - Automatic music downloader for SABnzbd
+#====================================================
+
+# clone headphones repo with git
+git clone https://github.com/rembo10/headphones.git headphones
+
+# move headphones into your home directory
+mv headphones ~/.headphones
+
+# move the startup script into place
+sudo mv ~/.headphones/init.ubuntu /etc/init.d/headphones
+
+# make the script executable
+sudo chmod +x /etc/init.d/headphones
+
+# edit the startup script and set some options
+sudo vim /etc/init.d/headphones
+
+# Change APP_PATH to /home/user/.headphones, where user is your username,
+# and change RUN_AS to your username
+
+# start headphones
+sudo service headphones start
+
+# browse to the headphones webpage
+# http://localhost:8181
