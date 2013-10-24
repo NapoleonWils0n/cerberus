fc5b2913b8290465377b8746c4be8e9920d33b57
Daniel J Wilcox <danieljwilcox@gmail.com>
2013-10-24 02:28:14 +0100

    vim powerline plugin install

--- linux-mint/xfce-linux-mint-macair-install.sh
+++ linux-mint/xfce-linux-mint-macair-install.sh
@@ -1252,3 +1252,66 @@ sudo service headphones start
 
 # browse to the headphones webpage
 # http://localhost:8181
+
+# vim powerline and fugitive plugin install
+#==========================================
+
+
+# install python-pip
+#===================
+sudo apt-get install python-pip
+
+# install powerline
+#==================
+pip install --user git+git://github.com/Lokaltog/powerline
+
+cd ~/Desktop
+
+wget https://github.com/Lokaltog/powerline/raw/develop/font/PowerlineSymbols.otf 
+wget https://github.com/Lokaltog/powerline/raw/develop/font/10-powerline-symbols.conf
+
+mkdir -p ~/.fonts/ && mv PowerlineSymbols.otf ~/.fonts/
+fc-cache -vf ~/.fonts
+mkdir -p ~/.config/fontconfig/conf.d/ && mv 10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
+
+# add the following to your ~/.vimrc
+#===================================
+set rtp+=$HOME/.local/lib/python2.7/site-packages/powerline/bindings/vim/
+
+" Always show statusline
+set laststatus=2
+
+" Use 256 colours (Use this setting only if your terminal supports 256 colours)
+set t_Co=256
+
+# copy theme to ~/.config/powerline
+#==================================
+mkdir -p ~/.config/powerline
+cp -R ~/.local/lib/python2.7/site-packages/powerline/config_files/* ~/.config/powerline
+
+# edit the ~/.config/powerline/config.json file 
+#==============================================
+
+# change the vim colour scheme to solarized
+# "vim": {
+# "colorscheme": "solarized",
+# }
+
+vim ~/.config/powerline/config.json
+
+# download the vim.fugitive plugin
+#=================================
+
+cd Desktop
+git clone git://github.com/tpope/vim-fugitive.git
+
+# move the files into place
+#==========================
+
+mv ~/Desktop/vim-fugitive/plugin/fugitive.vim ~/.vim/plugin
+mv ~/Desktop/vim-fugitive/doc/fugitive.txt ~/.vim/doc
+
+# unistall powerline
+#===================
+
+pip uninstall powerline
