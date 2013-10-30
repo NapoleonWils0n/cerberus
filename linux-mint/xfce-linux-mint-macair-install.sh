e9215739e05de50ec4884bb20bc2efed8714404b
Daniel J Wilcox <danieljwilcox@gmail.com>
2013-10-30 16:00:33 +0000

    tor install linux mint 15

--- linux-mint/xfce-linux-mint-macair-install.sh
+++ linux-mint/xfce-linux-mint-macair-install.sh
@@ -1315,3 +1315,27 @@ mv ~/Desktop/vim-fugitive/doc/fugitive.txt ~/.vim/doc
 #===================
 
 pip uninstall powerline
+
+
+
+# tor install linux mint 15
+#==========================
+
+# add tor repository to sources.list
+sudo vim /etc/apt/sources.list
+
+# add the following to your /etc/sources.list
+deb     http://deb.torproject.org/torproject.org raring main
+
+# add the gpg keys
+gpg --keyserver keys.gnupg.net --recv 886DDD89
+gpg --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | sudo apt-key add -
+
+# apt-get update
+sudo apt-get update
+
+# install the Debian package to help you keep our signing key current.
+sudo apt-get install deb.torproject.org-keyring
+
+# install tor
+sudo apt-get install tor
