a3ed3ba3bb61ca651cc3394d707646f1ad0a5826
Daniel J Wilcox <danieljwilcox@gmail.com>
2013-11-17 22:20:39 +0000

    lightum keyboard backlighting daemon

--- linux-mint/xfce-linux-mint-macair-install.sh
+++ linux-mint/xfce-linux-mint-macair-install.sh
@@ -1339,3 +1339,26 @@ sudo apt-get install deb.torproject.org-keyring
 
 # install tor
 sudo apt-get install tor
+
+
+# lightum keyboard backlighting daemon
+#=====================================
+
+sudo apt-get install build-essential
+
+# Clone the repository by running 
+git clone https://github.com/poliva/lightum 
+
+# cd into the lightum git repo
+cd lightum
+
+# Install the dependencies 
+sudo apt-get install libxss-dev libdbus-glib-1-dev
+
+# make
+make
+
+# sudo make install
+sudo make install
+
+# reboot and you should have a backlit keyboard
