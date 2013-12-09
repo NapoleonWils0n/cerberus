6aeaa9514b433de3b4368c4d180060040fdc1845
Daniel J Wilcox <danieljwilcox@gmail.com>
2013-12-09 19:28:24 +0000

    open sopcast stream on the ipad with xbmc

--- sopcast/sopcast-install.sh
+++ sopcast/sopcast-install.sh
@@ -17,18 +17,19 @@ tar -zxvf sopcast-player-0.8.5.tar.gz
 cd sopcast-player/
 
 
+# open port 6881 with iptables if you want to share the sopcast stream on your lan
 
 # play sopcast stream with mplayer
-(sp-sc sop://broker.sopcast.com:3912/143876 8901 9901 &>/dev/null &); sleep 10; wait $(mplayer -cache 8192 http://127.0.0.1:9901); killall sp-sc;
+(sp-sc sop://broker.sopcast.com:3912/143876 8901 6881 &>/dev/null &); sleep 10; wait $(mplayer -cache 8192 http://127.0.0.1:9901); killall sp-sc;
 
 # save sopcast stream with ffmpeg
-sp-sc sop://broker.sopcast.com:3912/143876 8901 9901 &>/dev/null &
+sp-sc sop://broker.sopcast.com:3912/143876 8901 6881 &>/dev/null &
 ffmpeg -i http://127.0.0.1:9901 -acodec copy -vcodec copy sopcast-stream.mkv
 
 # save sopcast stream with tv-maxe
 
 # play sopcast stream with vlc
-(sp-sc sop://broker.sopcast.com:3912/143876 8901 9901 &>/dev/null &); sleep 10; wait $(vlc http://127.0.0.1:9901); killall sp-sc;
+(sp-sc sop://broker.sopcast.com:3912/143876 8901 6881 &>/dev/null &); sleep 10; wait $(vlc http://127.0.0.1:9901); killall sp-sc;
 
 
 # sopcast mplayer function for your ~/.bashrc
@@ -37,7 +38,7 @@ ffmpeg -i http://127.0.0.1:9901 -acodec copy -vcodec copy sopcast-stream.mkv
 smplayer () {
         (sp-sc "$1" 8901 9901 &>/dev/null &); 
 	sleep 10; 
-	wait $(mplayer -cache 8192 http://localhost:9901); 
+	wait $(mplayer -cache 8192 http://localhost:6881); 
 	killall sp-sc;
 }
 
@@ -46,4 +47,13 @@ prefer-ipv4 = yes
 nolirc=yes
 
 
-
+# create a text file with a .strm extension
+# paste in the zero config address of your computer and port
+# http://horus.local:6881
+# in xbmc on your ipad go to videos, files add videos
+# click browse and select the home folder click media and then the downloads folder
+# click ok and add the downloads folder as a source
+# copy the sopcast.strm file with url and port number of sopcast stream on your computer to the downloads folder on the ipad
+# just plug the ipad into your linux laptop and it should mount and then you just copy the sopcast.strm file to the downloads folder on the ipad
+# in xbmc you should now see the sopcast.strm text file in the downloads source you created
+# select the sopcast.strm file and it will open the stream from your other computer and play the stream
