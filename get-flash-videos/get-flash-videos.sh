4c29916df22b6509006e68b8104d51f621985ee4
Daniel J Wilcox <danieljwilcox@gmail.com>
2013-12-16 15:54:17 +0000

    get flash videos install

---
+++ get-flash-videos/get-flash-videos.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# get-flash-videos install
+#=========================
+
+sudo apt-get install get-flash-videos
+
+# https://gitorious.org/get-flash-videos-plugins/pages/Hulu
+
+# install hulu plugin
+get_flash_videos --add-plugin https://gitorious.org/get-flash-videos-plugins/gfv-plugins/raw/e80935f0d40b878c8ef743435cecd5d30d084ee1:Hulu.pm
+
+
