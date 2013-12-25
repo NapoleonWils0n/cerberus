06cec9d09db75103107bfef6e17de3705fafbeb8
Daniel J Wilcox <danieljwilcox@gmail.com>
2013-12-25 20:00:13 +0000

    lynx browser install with ssl support

---
+++ lynx/lynx-install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# lynx browser install
+#=====================
+
+sudo apt-get install lynx
+
+# lynx config file:
+# ~/.lynx.cfg
+
+
+SET_COOKIES:FALSE
+ACCEPT_ALL_COOKIES:FALSE
+PERSISTENT_COOKIES:FALSE
+SSL_CERT_FILE:/etc/ssl/certs/ca-certificates.crt
+#COOKIE_FILE:~/.lynx_cookies
