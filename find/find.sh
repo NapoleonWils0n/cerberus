4eb87292a209919fdfbb73e6a8586d86eb915e04
Daniel J Wilcox <danieljwilcox@gmail.com>
2013-12-28 21:08:43 +0000

    find change permissions

--- find/find.sh
+++ find/find.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # The find utility can locate all of those files and then execute a command to move them where you want.
-
 find . -name '*.mp3' -print -exec mv '{}' ~/songs \;
 
 # find with odd characters
-
 find . -name '*.mp3' -print0 | xargs -i -0 mv '{}' ~/songs
+
+# find change permissions
+find some_directory -type f -print0 | xargs -0 chmod 0644
