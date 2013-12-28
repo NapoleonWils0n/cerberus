edd56aeed4ce51e1d72154c5a697601daa4d5035
Daniel J Wilcox <danieljwilcox@gmail.com>
2013-12-28 21:04:59 +0000

    find with odd characters

--- find/find.sh
+++ find/find.sh
@@ -3,3 +3,7 @@
 # The find utility can locate all of those files and then execute a command to move them where you want.
 
 find . -name '*.mp3' -print -exec mv '{}' ~/songs \;
+
+# find with odd characters
+
+find . -name '*.mp3' -print0 | xargs -i -0 mv '{}' ~/songs
