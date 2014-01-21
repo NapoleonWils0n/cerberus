b1090dc695111a52be0926ff4c6981e6d9ce0abb
Daniel J Wilcox <danieljwilcox@gmail.com>
2014-01-21 01:54:12 +0000

    iptables for mpd

--- linux-mint/xfce-linux-mint-macair-install.sh
+++ linux-mint/xfce-linux-mint-macair-install.sh
@@ -279,6 +279,7 @@ sudo iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 
 sudo iptables -A INPUT -i lo -j ACCEPT
 sudo iptables -A INPUT -p tcp --dport 80 -s 192.168.1.0/24 -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 6600 -s 192.168.1.0/24 -j ACCEPT
 sudo iptables -A INPUT -p tcp --dport 6881 -s 192.168.1.0/24 -j ACCEPT
 sudo iptables -A INPUT -p tcp --dport 6882 -s 192.168.1.0/24 -j ACCEPT
 sudo iptables -A INPUT -p tcp -m tcp --dport 5353 -s 192.168.1.0/24 -j ACCEPT
