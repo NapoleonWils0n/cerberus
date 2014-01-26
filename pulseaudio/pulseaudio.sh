446a0b42bc6c003aaf157b60c85d9a15d81427ec
Daniel J Wilcox <danieljwilcox@gmail.com>
2014-01-26 15:48:53 +0000

    pulseaudio sink

--- pulseaudio/pulseaudio.sh
+++ pulseaudio/pulseaudio.sh
@@ -1,23 +1,33 @@
 #!/bin/bash
 
+# pulseaudio sink
+#================
+
+sudo apt-get install pulseaudio-module-raop pulseaudio-module-zeroconf paprefs pavucontrol 
+
+pactl load-module module-raop-sink server=talos.local
+
 pactl stat
 
+# open port for pulseadio
 sudo iptables -A INPUT -p tcp --dport 4713 -s 192.168.1.0/24 -j ACCEPT
+
+# close port for pulseaudio
 sudo iptables -D INPUT -p tcp --dport 4713 -s 192.168.1.0/24 -j ACCEPT
 
+# list pulseaudio sources
+pacmd list-sources
 
-sudo iptables -A INPUT -p udp --dport 1900 -s 192.168.1.0/24 -j ACCEPT
-sudo iptables -D INPUT -p udp --dport 1900 -s 192.168.1.0/24 -j ACCEPT
-UDP port 1900
+# add to /etc/pulse/default.pa a following line:
+load-module module-http-protocol-tcp
 
+# restart pulseaudio
+pulseaudio -k 
 
-pacmd load-module module-http-protocol-tcp
-
-pacmd load-module module-zeroconf-publish
-
+# mpd.conf pulseaudio sink
 audio_output {
         type                    "pulse"
         name                    "audio-card-usb"
-	server                "remote_server"
-        sink                    "alsa_input.pci-0000_00_1b.0.analog-stereo"
+		  server                "remote_server"
+        sink                    "alsa_output.usb-Musical_Fidelity_Musical_Fidelity_V-Link_192kHz___24bit_0-00-M192kHz.analog-stereo.monitor"
 }
