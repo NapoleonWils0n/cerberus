bdce2c9cb2143bb7eb5eda1453efc7fcb2e2d09a
Daniel J Wilcox <danieljwilcox@gmail.com>
2014-02-18 23:56:06 +0000

    command split onto 2 lines

--- find/find-regex-convert-images.sh
+++ find/find-regex-convert-images.sh
@@ -3,7 +3,8 @@
 # find multiple image files with regex and then convert to png
 #=============================================================
 
-find . -type f -regex ".*\.\(gif\|jpg\|jpeg\)$" -exec convert '{}' -set filename:fname '%t' +adjoin '%[filename:fname].png' \;
+find . -type f -regex ".*\.\(gif\|jpg\|jpeg\)$" \
+-exec convert '{}' -set filename:fname '%t' +adjoin '%[filename:fname].png' \;
 
 
 # delete the original images files
