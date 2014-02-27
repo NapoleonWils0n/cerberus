02cafff32f3ce4414369c45c6521331818391b2e
Daniel J Wilcox <danieljwilcox@gmail.com>
2014-02-27 17:47:52 +0000

    find htl pages extract content re save

--- ebooks/making-ebooks-from-html.sh
+++ ebooks/making-ebooks-from-html.sh
@@ -59,6 +59,12 @@ find . -type f -regex ".*\.\(htm\|html\)$" -exec sed -i 's/\.\(jpe\?g\|gif\)/\.p
 sudo apt-get install html-xml-utils
 
 
+# install tidy for cleaning html
+#===============================
+
+sudo apt-get install tidy
+
+
 # strip out just the content to paragraphs - makingnetwork.org
 #======================================================================
 
@@ -67,10 +73,16 @@ hxselect -s '\n' -c  \
 'html>body>table>tbody>tr>td:nth-of-type(2)>table>tbody>tr:nth-of-type(5)>td>table>tbody>tr>td' > clean-index.htm
 
 
-# install tidy for cleaning html
-#===============================
+# find html pages strip out the content, tidy the html re add the html, head and body tags and save the file
+find . -type f -regex ".*\.\(htm\|html\)$" |
+while read file
+do
+hxnormalize -x "$file" | \
+hxselect -s '\n' -c  \
+'html>body>table>tbody>tr>td:nth-of-type(2)>table>tbody>tr:nth-of-type(5)>td>table>tbody>tr>td' | \
+tidy -mibq --doctype strict --drop-font-tags yes --tidy-mark no --output-xhtml yes -o "$file"
+done
 
-sudo apt-get install tidy
 
 
 # tidy html remove font tags
@@ -86,20 +98,6 @@ tidy -mibq --doctype strict --drop-font-tags yes --tidy-mark no --output-xhtml y
 # --doctype strict = strict xhtml doctype
 # --drop-font-tags yes = remove font tags
 # --tidy-mark no = dont show tidy text
-# --output-xhtml yes = output closing tags
-
-# tidy remove font tags and dont output html head and body tags
-tidy -mibq -omit --doctype omit --drop-font-tags yes --tidy-mark no --show-body-only yes --output-xhtml yes clean-index.htm
-
-# m = modify original file
-# i = indent
-# b = bare strip quotes
-# q = quiet
-# -omit = omit optional end tags
-# --doctype omit = omit doctype
-# --drop-font-tags yes = remove font tags
-# --tidy-mark no = dont show tidy text
-# --show-body-only yes = only output text between body tags
 # --output-xhtml yes = output closing tags
 
 #=============================================================
