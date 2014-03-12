cfb3247669381a8abcf51656d2fd4813ad1242b9
Daniel J Wilcox <danieljwilcox@gmail.com>
2014-03-12 02:42:54 +0000

    latex install fonts

---
+++ latex/latex-install-fonts.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# install non free fonts for latex
+#=================================
+
+wget http://www.tug.org/fonts/getnonfreefonts/install-getnonfreefonts
+chmod +x install-getnonfreefonts
+sudo ./install-getnonfreefonts
+sudo getnonfreefonts-sys -a
+
+# install t1 fonts
+sudo apt-get install cm-super
