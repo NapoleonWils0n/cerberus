dc1fa0d949463bf990e3d6a870d93c5165ef1e0a
Daniel J Wilcox <danieljwilcox@gmail.com>
2014-05-04 20:35:55 +0100

    xbmc repo for linux

---
+++ xbmc/xbmc-linux-install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# xbmc linux install
+#===================
+
+sudo apt-get install python-software-properties pkg-config
+sudo apt-get install software-properties-common
+sudo add-apt-repository ppa:team-xbmc/ppa
+sudo apt-get update
+sudo apt-get install xbmc
