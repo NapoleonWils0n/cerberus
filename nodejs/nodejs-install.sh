b6112f557fa333b6b6010235cadd9183795b64e8
Daniel J Wilcox <danieljwilcox@gmail.com>
2014-06-12 22:47:29 +0100

    nodejs install with ppa

---
+++ nodejs/nodejs-install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# nodejs install 
+#===============
+
+sudo apt-get update
+sudo apt-get install -y python-software-properties python g++ make
+sudo add-apt-repository ppa:chris-lea/node.js
+sudo apt-get update
+sudo apt-get install nodejs
