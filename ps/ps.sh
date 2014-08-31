7b20b4c820da6e0d39968af2f42723d7553758a3
Daniel J Wilcox <danieljwilcox@gmail.com>
2014-08-31 17:07:35 +0100

    ps command

---
+++ ps/ps.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# ps command
+#===========
+
+# display information of processes for a specified username
+ps -u
+
+# display all the processes in the system in full detail
+ps -ef
+
+# display one line of information for every thread 
+ps -eLf
+
+# display all processes of all users
+ps aux
+
+# specify which attributes you want to view
+ps axo
