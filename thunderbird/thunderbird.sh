0f39b87b42338ff81143126b70c33184eb18026b
Daniel J Wilcox <danieljwilcox@gmail.com>
2014-09-22 15:34:09 +0100

    thunderbirds are go

---
+++ thunderbird/thunderbird.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# thunderbird install
+#====================
+
+sudo pacman -S thunderbird
+
+#Optional dependencies for thunderbird
+#    libcanberra: for sound support
+
+# fullscreen
+# alt fn F11
+ 
