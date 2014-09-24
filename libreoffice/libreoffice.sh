354ba8bfece088236c1d5b707e4d083de9763036
Daniel J Wilcox <danieljwilcox@gmail.com>
2014-09-24 15:04:37 +0100

    libreoffice install

---
+++ libreoffice/libreoffice.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# libreofffice install
+#=====================
+
+# install libreoffice
+sudo pacman -S libreoffice-fresh
+
+# install uk language pack
+sudo pacman -S libreoffice-fresh-en-GB
