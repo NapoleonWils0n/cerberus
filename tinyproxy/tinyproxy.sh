0fe68eb1523bb73f68aed54ab530c04a9adfd9c1
Daniel J Wilcox <danieljwilcox@gmail.com>
2014-10-25 23:09:35 +0100

    tinyproxy install and config

---
+++ tinyproxy/tinyproxy.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# tinyproxy install and config
+#=============================
+
+sudo pacman -S tinyproxy
+
+# edit the tinyproxy config file
+sudo vim /etc/tinyproxy/tinyproxy.conf
+
+# change the following options in the config file
+#================================================
+
+# change the port to one open in your firewall
+Port 6881
+
+# comment out Listen 127.0.0.1 so you can access the proxy on the lan network
+
+#Listen 127.0.0.1
+
+# Allow access for an ip address
+Allow 192.168.1.4
+
+# Allow access from the local subnet
+Allow 192.168.1.0/24
+
+
+
+# start tinyproxy
+sudo systemctl start tinyproxy
+
+# stop tinyproxy
+sudo systemctl stop tinyproxy
