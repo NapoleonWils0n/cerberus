a82bbf94e4300d2ee4743496a31bd3cf93b78015
Daniel J Wilcox <danieljwilcox@gmail.com>
2014-10-30 17:19:28 +0000

    changed command

--- ffmpeg/ffmpeg-record-screen-and-mic.sh
+++ ffmpeg/ffmpeg-record-screen-and-mic.sh
@@ -6,7 +6,7 @@
 
 
 # record screen and mic in mono
-ffmpeg -f x11grab -video_size 1366x768 -i $DISPLAY -f alsa -ac 1 -i hw:0,0 -c:v ffvhuff -c:a flac test.mkv
+ffmpeg -f x11grab -video_size 1366x768 -i $DISPLAY -f alsa -i hw:0,0 -c:v ffvhuff -c:a flac test.mkv
 
 # record screen and mic in stereo
 ffmpeg -f x11grab -video_size 1366x768 -i $DISPLAY -f alsa -ac 2 -i hw:0,0 -c:v ffvhuff -c:a flac test.mkv
