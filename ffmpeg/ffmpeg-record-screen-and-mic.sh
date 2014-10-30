933063233d2c3391dd5c76eb2dc6210c420440a3
Daniel J Wilcox <danieljwilcox@gmail.com>
2014-10-30 16:04:42 +0000

    ffmpeg record screen and mic

---
+++ ffmpeg/ffmpeg-record-screen-and-mic.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+# ffmeg record screen and mic
+#============================
+
+
+# record screen and mic in mono
+ffmpeg -f x11grab -video_size 1366x768 -i $DISPLAY -f alsa -ac 1 -i hw:0,0 -c:v ffvhuff -c:a flac test.mkv
+
+# record screen and mic in stereo
+ffmpeg -f x11grab -video_size 1366x768 -i $DISPLAY -f alsa -ac 2 -i hw:0,0 -c:v ffvhuff -c:a flac test.mkv
