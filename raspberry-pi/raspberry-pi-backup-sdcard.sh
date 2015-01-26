c07d045ec5068b90955ac256863c4b02450f3089
Daniel J Wilcox <danieljwilcox@gmail.com>
2015-01-26 19:25:25 +0000

    back up sdcard

---
+++ raspberry-pi/raspberry-pi-backup-sdcard.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+
+# rapsberry pi backup sdcard
+#===========================
+
+# install pv from aur for a progress bar with dd
+
+yaourt -S pv
+
+
+# backup sdcard to image
+#=======================
+
+df -h
+
+sudo su
+
+dd bs=4M if=/dev/sdb | pv -trab | dd bs=4M  of=/home/djwilcox/Desktop/raspberry_pi-$(date +"%H-%M-%m-%d-%y").img
+
+
+# restore image to sdcard
+#=========================
+
+df -h
+
+
+sudo su
+dd bs=4M if=raspberry_pi-14-34-01-24-15.img | pv -trab | dd bs=4M of=/dev/sdb 
