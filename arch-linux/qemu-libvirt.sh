0e0dd6e49e096316c8ea243b0d764df8b8a2b6d8
Daniel J Wilcox <danieljwilcox@gmail.com>
2015-02-17 01:39:58 +0000

    libvrt qemu set up

--- arch-linux/qemu-libvirt.sh
+++ arch-linux/qemu-libvirt.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-# qemu set up
-#============
+
+# qemu install
+#===============================================
 
 sudo pacman -Ss qemu libvirt bridge-utils openbsd-netcat virt-manager 
+
 
 # check hardware support
 lscpu
@@ -20,18 +22,21 @@ zgrep CONFIG_VIRTIO /proc/config.gz
 lsmod | grep kvm
 lsmod | grep virtio
 
-# In case the above commands return nothing, you need to load kernel modules
+
+# In case the above commands return nothing, 
+# you need to load kernel modules
 
 
-#==========================================================================
-
-#  Run and enable boot up start libvirtd daemon:
-
-sudo systemctl start libvirtd
-sudo systemctl enable libvirtd
 
 
-# Use PolicyKit authorization create /etc/polkit-1/rules.d/49-org.libvirt.unix.manager.rules
+
+
+# Create PolicyKit authorization file for kvm group
+#==================================================
+
+
+# create /etc/polkit-1/rules.d/49-org.libvirt.unix.manager.rules
+
 
 sudo vim /etc/polkit-1/rules.d/49-org.libvirt.unix.manager.rules
 
@@ -46,147 +51,201 @@ polkit.addRule(function(action, subject) {
 });
 
 
-#==========================================================================
 
-# Then restart polkit service.
-
+# restart polkit service
+#===============================================
 
 sudo systemctl restart polkit.service
 
-#==========================================================================
 
 
-# You will need to create the libvirt or kvm group and add any users you want to have access to libvirt or kvm to that group:
+# create the kvm group and add your user to it
+#===============================================
 
+# replace username with your username
 
 sudo groupadd kvm
-sudo gpasswd -a djwilcox kvm
+sudo gpasswd -a username kvm
 
-#==========================================================================
 
+
+
+
+# set up networking
+#===============================================
 
 # check ethernet device name
 
 ip a
 
-
 # the ethernet device is usually called eth0
 # my ethernet device is called ens9
 
-# if your ethernet is called eth0, replace ens9 with eth0 in the commands below
+# if your ethernet is called eth0, 
+# replace ens9 with eth0 in the commands below
+
+
 
 
 # set ethernet device down 
-#=========================
 
 sudo ip link set dev ens9 down
 
 
-# create bridge
-#==============
+
+# create the bridge
 
 sudo brctl addbr br0
 
 
+
 # Add a device to a bridge
-#=========================
 
 sudo brctl addif br0 ens9
 
 
+
 # show bridges
-#=============
 
 brctl show
 
 
+
 # set ethernet device up 
-#=========================
 
 sudo ip link set up dev ens9
 
 
+
 # Set the bridge device up:
-#=========================
 
 sudo ip link set up dev br0
 
 
 
-# Delete a bridge, you need to first set it to down:
-#===================================================
+# Delete a bridge, 
+# you need to first set it to down:
+
 
 sudo ip link set dev br0 down
 sudo brctl delbr br0
 
 
-# Enable IPv4 forwarding:
-#========================
-
-sudo sysctl net.ipv4.ip_forward=1
-
-
 
 # Allow qemu to access to the bridge br0
-#=======================================
+#===============================================
 
+
+# copy the bridge.conf.sample and rename to bridge.conf
 
 sudo cp /etc/qemu/bridge.conf.sample /etc/qemu/bridge.conf
 
 
 
-# test if libvirt is working properly 
-#=============================================================
+# Enable IPv4 forwarding:
+#===============================================
+
+sudo sysctl net.ipv4.ip_forward=1
+
+
+
+
+
+# start / enable libvirtd at boot
+#===============================================
+
+
+# start libvirtd 
+
+sudo systemctl start libvirtd
+
+
+# enable libvirtd at boot
+
+sudo systemctl enable libvirtd
+
+
+
+
+
+# Test if libvirt is working properly 
+#===============================================
+
 
 # To test if the daemon is working properly on a system level:
 
 virsh -c qemu:///system
 
 
-# To test if libvirt is working properly for a user-session: 
+# the virsh prompt looks like this:
 
+virsh #
+
+
+
+
+# To test if libvirt is working properly for a user-session: 
 
 virsh -c qemu:///session
 
 
-# home directory file permissions
-#=============================================================
 
-chmod o+x /home/djwilcox
+
+# create a storage pool file directory in your home directory
+#============================================================
+
+
+# create a directory for the libvirt storage pool
+# replace username with your username
+
+mkdir -p /home/username/libvirt/images
+
+
+# chown the libvirt directory to allow both your user and the kvm group access
+# replace username with your username
+
+sudo chown -R username:kvm /home/username/libvirt
+
+
+
+# home directory file permissions
+#===============================================
+
+chmod o+x /home/username
+
 
 
 
 # change libvirt default storage pool directory
-#==============================================
+#===============================================
 
 
-# switch to root
+# list pools
 
-sudo su
+sudo virsh pool-list --all
 
 
 # virsh pool-dumpxml
 
-virsh pool-dumpxml default > default.xml 
+sudo virsh pool-dumpxml default 
 
 
-# the default.xml file will be owned with root, so we need to change ownership before we can edit it
+# Stopping a Pool
 
-sudo chown djwilcox:users default.xml 
-
-
-
-# Remove the current default pool
-
-virsh pool-destroy default 
+sudo virsh pool-destroy poolname
 
 
+# undefine a Pool
 
-# Now create a new storage pool based on the updated XML file
-
-virsh pool-create pool.xml
+sudo virsh pool-undefine poolname
 
 
-virsh pool-refresh name
+# Starting a Pool
 
+sudo virsh pool-start poolname
+
+
+# Enable Autostarting a Pool
+
+sudo virsh pool-autostart poolname
 
