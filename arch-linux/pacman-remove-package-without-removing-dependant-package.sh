a0ecd64e7ea96e2a87fc890919d09ecf15e4f4d3
Daniel J Wilcox <danieljwilcox@gmail.com>
2015-02-25 15:50:31 +0000

    pacman remove package without removing dependant

---
+++ arch-linux/pacman-remove-package-without-removing-dependant-package.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+# To remove a package, which is required by another package, without removing the dependent package: 
+
+sudo pacman -Rdd package-name
+
+
+sudo pacman -Rdd utvideo-git
