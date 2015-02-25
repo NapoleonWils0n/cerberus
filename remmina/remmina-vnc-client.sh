c3fe2a07fc8783ff1d87f3cdba7d51b87c5671f2
Daniel J Wilcox <danieljwilcox@gmail.com>
2015-02-25 01:43:44 +0000

    remmina vnc client

---
+++ remmina/remmina-vnc-client.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# remmina vnc client install
+#===========================
+
+
+sudo pacman -S freerdp libvncserver remmina
+
+
+
+# install veency from cydia on ios
+
+# select new connection and select vnc
+# change the colour depth from 256 colours to high colour (15 bit)
