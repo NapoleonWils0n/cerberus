3aac826b8709388b1d747d301e0afa3907d675d0
Daniel J Wilcox <danieljwilcox@gmail.com>
2015-03-09 17:33:12 +0000

    apple super drive arch magic packet

---
+++ arch-linux/arch-linux-apple-usb-superdrive.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# arch linux apple super drive magic packet
+#==============================================
+
+
+# install xfburn
+
+sudo pacman -S xfburn
+
+
+# ls devices
+ls /dev
+
+# the drive should be listed as sr0 or sr1
+
+# send the magic packet replace sr0 with your drive
+sg_raw /dev/sr0 EA 00 00 00 00 00 01
+
+# custom udev rule send magic packet when drive is plugged in
+sudo vim /etc/udev/rules.d/99-local.rules
+
+# add the code below to /etc/udev/rules.d/99-local.rules
+
+ACTION=="add", ATTRS{idProduct}=="1500", ATTRS{idVendor}=="05ac", DRIVERS=="usb", RUN+="/usr/bin/sg_raw /dev/$kernel EA 00 00 00 00 00 01"
