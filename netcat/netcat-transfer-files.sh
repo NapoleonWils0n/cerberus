7b0d69b2cf3c45d1de55e8cf1efe08b02e710313
Daniel J Wilcox <danieljwilcox@gmail.com>
2015-03-22 21:25:45 +0000

    netcat file transfer

--- netcat/netcat-transfer-files.sh
+++ netcat/netcat-transfer-files.sh
@@ -3,3 +3,10 @@
 # transfer files
 nc -l 3333 < backup.iso
 nc 192.168.1.5 3333 > backup.iso
+
+
+# client listen to recieve file
+nc -v -w 30 6881 -l > wireshark.pdf
+
+# server sending file
+nc -v -w 2 192.168.1.3 6881 > wireshark.pdf
