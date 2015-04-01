fa8c6484785e508c5b3af930537b6b22dd7b814b
Daniel J Wilcox <danieljwilcox@gmail.com>
2015-04-01 22:58:27 +0100

    openvpn up script

---
+++ openvpn/openvpn-up.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+# openvpn-up.sh
+#==============
+
+# create vpn route
+/usr/bin/ip rule add from "$ifconfig_local" table tunnel
+/usr/bin/ip route add table tunnel default via "$ifconfig_remote" 
+/usr/bin/ip route add table tunnel "$ifconfig_remote" via "$ifconfig_local" dev "$dev"
+
+# start socks5 proxy server
+/usr/bin/systemctl start sockd.service
+
+# echo tun0 ip address to unbound_outgoing_interface
+/usr/bin/echo "outgoing-interface: $(ip a list tun0 \
+| grep inet | head -1 | sed 's/\:/ /' | \
+awk '{print $2}')" > /etc/unbound/unbound_outgoing_interface
+
+# restart unbound dns server
+/usr/bin/systemctl restart unbound.service
