8893aa9633dc8f78aa07fb6cf76d9bfd1a7bdbb9
Daniel J Wilcox <danieljwilcox@gmail.com>
2015-04-20 14:58:59 +0100

    wget download site

--- wget/wget-download-from-archive-org.sh
+++ wget/wget-download-from-archive-org.sh
@@ -5,5 +5,7 @@
 wget \
 -np -e robots=off \
 --mirror \
+--convert-links \
+--page-requisites \
 --domains=staticweb.archive.org,web.archive.org \
 http://web.archive.org/web/19970531012829/http://www.communities.org.uk/
