7d6bab6cf5c55d03b6a60a4041ca98529768a214
Daniel J Wilcox <danieljwilcox@gmail.com>
2015-05-19 02:07:36 +0100

    find and convert iconv

--- iconv/iconv-convert-latin-to-utf-8.sh
+++ iconv/iconv-convert-latin-to-utf-8.sh
@@ -2,3 +2,7 @@
 
 # convert from latin to utf-8
 iconv -f iso-8859-1 -t utf-8 infile.htm -o outfile.htm
+
+# find and convert
+find . -type f -regex ".*\.\(htm\|html\)$" \
+-exec iconv -f iso-8859-1 -t utf-8 '{}' -o '{}' \;
