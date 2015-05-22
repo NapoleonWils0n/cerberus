a65d119e5092128d9d985c8c901a82967a9c370d
Daniel J Wilcox <danieljwilcox@gmail.com>
2015-05-22 23:15:41 +0100

    linkchecker perl-io-html check local pages

--- linkchecker/linkchecker.sh
+++ linkchecker/linkchecker.sh
@@ -10,8 +10,14 @@ yaourt -S perl-w3c-linkchecker
 sudo pacman -S perl-lwp-protocol-https
 
 
+# install perl-io-html to check local html pages
+sudo pacman -S perl-io-html
+
 # check links output as html
 checklink -r -D 10 -i -H http://mediablends.org.uk | tee links.html
 
 # check link output as text
 checklink -r -D 10 -s http://mediablends.org.uk | tee links.txt
+
+# check local links output as text
+checklink -r -D 10 -s wiki | tee links.txt
