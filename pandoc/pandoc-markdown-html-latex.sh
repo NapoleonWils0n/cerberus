b9959bd65b7c44446a9ca3cf1a9640fd5ac7db88
Daniel J Wilcox <danieljwilcox@gmail.com>
2015-06-23 23:27:00 +0100

    find and convert

--- pandoc/pandoc-markdown-html-latex.sh
+++ pandoc/pandoc-markdown-html-latex.sh
@@ -93,3 +93,36 @@ do
 filebasename=`echo $file | sed 's/\.\md/.tex/g'`
 pandoc --self-contained -s -S -N --normalize --toc -o "$filebasename" "$file"
 done
+
+
+
+
+# pandoc find markdown files and convert into 1 latex file
+#=========================================================
+
+
+pandoc --self-contained -s -S --normalize --toc \
+../metadata.yaml \
+-o outfile.tex  \
+$(find . -type f -regex ".*\.md$" -print)
+
+
+
+# pandoc find markdown files and convert into 1 pdf
+#==================================================
+
+pandoc --self-contained -s -S --normalize --toc \
+../metadata.yaml \
+-o outfile.pdf \
+$(find . -type f -regex ".*\.md$" -print)
+
+
+
+# pandoc find markdown files and convert into 1 epub
+#===================================================
+
+pandoc --self-contained -s -S --normalize --toc \
+../metadata.yaml \
+-o outfile.epub \
+$(find . -type f -regex ".*\.md$" -print)
+
