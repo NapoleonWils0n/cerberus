f7cfd83c2b6ec20833af200e6305824bf19e6c11
Daniel J Wilcox <danieljwilcox@gmail.com>
2015-06-23 16:13:10 +0100

    find convert to latex

--- pandoc/pandoc-find-files-convert-to-pdf-ebook.sh
+++ pandoc/pandoc-find-files-convert-to-pdf-ebook.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 
-
-# pandoc find markdown files and convert into 1 pdf
+# pandoc find markdown files and convert into latex
 #==================================================
 
 
 pandoc --self-contained -s -S --normalize --toc \
 ../metadata.yaml \
--o networks.pdf \
+-o outfile.tex  \
 $(find . -type f -regex ".*\.md$" -print)
 
+# pandoc find markdown files and convert into 1 pdf
+#==================================================
 
+pandoc --self-contained -s -S --normalize --toc \
+../metadata.yaml \
+-o outfile.pdf \
+$(find . -type f -regex ".*\.md$" -print)
 
 
 # pandoc find markdown files and convert into 1 epub
 #===================================================
 
-
 pandoc --self-contained -s -S --normalize --toc \
 ../metadata.yaml \
--o networks.epub \
+-o outfile.epub \
 $(find . -type f -regex ".*\.md$" -print)
