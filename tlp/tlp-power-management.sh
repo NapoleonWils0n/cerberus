ddb99b48a662286704e766e85f871c0e0c34f626
Daniel J Wilcox <danieljwilcox@gmail.com>
2015-07-27 15:36:41 +0100

    tlp set up

--- tlp/tlp-power-management.sh
+++ tlp/tlp-power-management.sh
@@ -5,7 +5,13 @@
 
 sudo pacman -S tlp tlp-rdw
 
+# mask systemd-rfkill
+sudo systemctl mask systemd-rfkill@.service
+
 # To load TLP, enable and start the systemd units: tlp.service and tlp-sleep.service. 
+
+# enable services
+sudo systemctl enable tlp.service tlp-sleep.service
 
 # start tlp 
 sudo systemctl start tlp tlp-sleep
