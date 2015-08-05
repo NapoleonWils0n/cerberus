62186f15c7468eae0f1da1bd6b925e35991eed8b
Daniel J Wilcox <danieljwilcox@gmail.com>
2015-08-05 14:17:53 +0100

    get iplayer arch install

---
+++ arch-linux/get-iplayer-arch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# get_iplayer arch linux install
+#===============================
+
+# install get_iplayer
+
+yaourt -S get_iplayer
+
+
+# install perl-xml-simple
+
+sudo pacman -S perl-xml-simple
