e4062eb82bb869101e6a9b416f4eac9250c99627
Daniel J Wilcox <danieljwilcox@gmail.com>
2015-08-20 23:51:06 +0100

    set mpv to open youtube videos

--- newsbeuter/newsbeuter.sh
+++ newsbeuter/newsbeuter.sh
@@ -17,9 +17,15 @@ touch ~/.newsbeuter/config
 
 
 # edit ~/.newsbeuter/config
+
 # set the browser
+# if you subscribe to a youtube channel and you would like to open the video with mpv, 
+# add the macro, to use a macro, you must first press the , key, 
+# followed by the keybind. In the example above, you would type , + y
+
 
 vim  ~/.newsbeuter/config
 
 auto-reload yes
 browser "/usr/bin/chromium %u"
+macro y set browser "/usr/bin/mpv %u"; open-in-browser ; set browser "/usr/bin/chromium %u"
