f01dc0f8ab59d43742093cb75fdedc2bba21367b
Daniel J Wilcox <danieljwilcox@gmail.com>
2015-08-20 21:08:24 +0100

    rsync ssh calibre cops

--- cops/cops-ebook-server.sh
+++ cops/cops-ebook-server.sh
@@ -105,3 +105,34 @@ $config['cops_use_url_rewriting'] = "1";
 
 
 # copy calibre libray to /var/www/calibre with rsync over ssh
+
+
+
+# rsync ssh sync local calibre library to cops ebook server
+#===========================================================
+
+
+# copy your ssh keys to the server
+
+
+# set up ssh config for the server 
+vim ~/.ssh/config
+
+
+# cops server example ssh config
+Host cops
+User root
+Port 22
+HostName 10.100.8.90
+
+
+
+# rsync ssh dry run
+rsync -avz -e ssh --dry-run --progress --delete /home/username/Documents/calibre/ cops:/var/www/html/calibre/
+
+
+# rsync ssh 
+rsync -avz -e ssh --progress --delete /home/username/Documents/calibre/ cops:/var/www/html/calibre/
+
+
+
