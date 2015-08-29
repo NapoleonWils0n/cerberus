192c973e1c5d988edfcb9fba0b1419951bc9ff54
Daniel J Wilcox <danieljwilcox@gmail.com>
2015-08-29 16:17:33 +0100

    bashrc abd ffplay

--- ffplay/ffplay-alsa-driver-and-device.sh
+++ ffplay/ffplay-alsa-driver-and-device.sh
@@ -12,3 +12,12 @@ export AUDIODEV="plughw:1,0"
 # unset 
 unset SDL_AUDIODRIVER
 unset AUDIODEV
+
+
+# set SDL_AUDIODRIVER and AUDIODEV in your ~/.bashrc
+#====================================================
+
+vim ~/.bashrc
+
+export SDL_AUDIODRIVER="alsa"
+export AUDIODEV="plughw:1,0"
