11eeda6474822a84aae7ef9531eb6b1e631f82af
Daniel J Wilcox <danieljwilcox@gmail.com>
2016-06-05 15:52:36 +0100

    remind and wyrd

---
+++ remind/wyrd-remind.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+sudo pacman -S wyrd remind
+
+# copy config
+cp /etc/wyrdrc ~/.wyrdrc
+
+# create reminders directory
+mkdir ~/.reminders
+
+# create the reminders.rem file
+touch ~/.reminders/reminders.rem
+
+# edit config
+vim ~/.wyrdrc
+
+# change $EDITOR to gvim -v
+
+# command for editing an old appointment, given a line number %line% and filename %file%
+set edit_old_command="$EDITOR +%line% \"%file%\""
+# command for editing a new appointment, given a filename %file%
+set edit_new_command="$EDITOR +999999 \"%file%\""
+# command for free editing of the reminders file, given a filename %file%
+set edit_any_command="$EDITOR \"%file%\""
+
+# command for editing an old appointment, given a line number %line% and filename %file%
+set edit_old_command="gvim -v +%line% \"%file%\""
+# command for editing a new appointment, given a filename %file%
+set edit_new_command="gvim -v +999999 \"%file%\""
+# command for free editing of the reminders file, given a filename %file%
+set edit_any_command="gvim -v \"%file%\""
+
+
