60968077ffba74394f820ce147c5775982579e7f
Daniel J Wilcox <danieljwilcox@gmail.com>
2019-02-18 23:26:13 +0000

    72 hours epg

--- tvheadend/tvheadend-linux/scripts/cron.sh
+++ tvheadend/tvheadend-linux/scripts/cron.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-/usr/bin/curl -s http://www.xmltv.co.uk/feed/6721 -o /home/hts/.xmltv/channels.xmltv && \
+# 72 hours epg
+
+/usr/bin/curl -s http://www.xmltv.co.uk/feed/6715 -o /home/hts/.xmltv/channels.xmltv && \
 /bin/cat /home/hts/.xmltv/channels.xmltv | /usr/bin/socat - UNIX-CONNECT:/home/hts/.hts/tvheadend/epggrab/xmltv.sock
