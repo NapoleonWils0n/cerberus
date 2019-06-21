4a1bf5795d93b82a347119251888458a7aafd555
Daniel J Wilcox <danieljwilcox@gmail.com>
2019-06-22 00:49:16 +0100

    ffmpeg create clip

--- ffmpeg/ffmpeg create clip.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# create a clip from a video with ffmpeg and convert to x264 aac
-
-ffmpeg -ss 00:00:00 -t 00:01:30 -i infile.avi -async 1 -vcodec libx264 -acodec libfaac -f mp4 out.mp4
