8df74727b525224e991337760caca1f6e1446b68
Daniel J Wilcox <danieljwilcox@gmail.com>
2019-06-22 15:14:55 +0100

    yuv pipe

--- ffmpeg/yuv4mpegpipe.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# ====================================
-# = Change the frame rate of a video =
-# ====================================
-
-ffmpeg -i input.flv -f yuv4mpegpipe - | yuvfps -s 25:1 -r 25:1  | ffmpeg -f yuv4mpegpipe -i - -b 28800k -y output25fps.avi
-
-
