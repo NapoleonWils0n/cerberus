4c3f244f464df7ad4656b4569b4456ef905f5d44
Daniel J Wilcox <danieljwilcox@gmail.com>
2021-05-25 18:54:54 +0100

    org

--- chown/chown-set-drive-permission-on-drive.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# set write permissions on mounted drive for current user
-#========================================================
-
-cd /media/username/drivename
-sudo chown -R $USER .
