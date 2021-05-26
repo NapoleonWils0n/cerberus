fe992ed68fe1a330baae254669911dc7ac7890d9
Daniel J Wilcox <danieljwilcox@gmail.com>
2021-05-26 22:58:48 +0100

    pandoc

--- pandoc/metadata-yaml.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-
-# markdown to pdf with metadata yaml file
-
-pandoc --self-contained -s -S --normalize --toc metadata.yaml -o outfile.pdf infile.md 
--- pandoc/pandoc-templates-setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# copy pandoc templates to home directory
-#========================================
-
-cp -R /usr/share/pandoc-1.10.1/data ~/.pandoc
