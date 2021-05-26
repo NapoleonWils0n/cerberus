30d4c44c2d9aa283637743305794fe851f68ad60
Daniel J Wilcox <danieljwilcox@gmail.com>
2021-05-26 20:49:48 +0100

    ssh

--- ssh/meld_over_ssh.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
- # ==============================================
- # = Using meld to compare directories over ssh =
- # ==============================================
-
-# Set up a ssh public private key and copy to your server
-
-meld <(ssh 192.168.1.2 ls Desktop) <(ls Desktop)
