5b6bf2c8dcd0d5c8fbaa785bb986aab732c9e0ea
Daniel J Wilcox <danieljwilcox@gmail.com>
2021-05-26 21:01:55 +0100

    ssh

--- ssh/regenerate_a_public_ssh-key_using_private_ssh-key.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# =====================================================
-# = REGENERATE A PUBLIC SSH-KEY USING PRIVATE SSH-KEY =
-# =====================================================
-
-
-ssh-keygen -y -f id_rsa > id_rsa.pub
--- ssh/ssh_afp.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# self closing ssh tunnel for afp file sharing
-
-SSHSERVER=80.100.10.4
-
-ssh -f -q -L 1548:localhost:548 $SSHSERVER sleep 60 && open afp://localhost:1548
