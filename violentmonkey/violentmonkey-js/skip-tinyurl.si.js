2077221ecb90c2629085b49209207ffeae0083cb
Daniel J Wilcox <danieljwilcox@gmail.com>
2022-09-17 21:14:36 +0100

    verion 1.1

--- violentmonkey/violentmonkey-js/skip-tinyurl.si.js
+++ violentmonkey/violentmonkey-js/skip-tinyurl.si.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Skip tinyurl.is
 // @namespace    Violentmonkey Scripts
-// @version      1.0
+// @version      1.1
 // @description  Skip tinyurl.is redirects
 // @author       Luke Pothier <lukepothier@gmail.com>
 // @match        https://tinyurl.is/*
