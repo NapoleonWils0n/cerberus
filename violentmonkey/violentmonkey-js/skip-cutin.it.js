62aafdc1a4d9889f8f70fad304d2a6673ee9d481
Daniel J Wilcox <danieljwilcox@gmail.com>
2022-11-06 15:37:55 +0000

    cutin

--- violentmonkey/violentmonkey-js/skip-cutin.it.js
+++ violentmonkey/violentmonkey-js/skip-cutin.it.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Skip cutin.it
 // @namespace    Violentmonkey Scripts
-// @version      1.0
+// @version      1.1
 // @description  Skip tinyurl.is redirects
 // @author       Luke Pothier <lukepothier@gmail.com>
 // @match        https://cutin.it/*
