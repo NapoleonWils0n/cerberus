a2b50559026e5fe3de4f51044ac0e3ee762ad559
Daniel J Wilcox <danieljwilcox@gmail.com>
2024-02-19 14:46:27 +0000

    dwl

---
+++ dwl/dwl-freebsd-config/config.mk
@@ -0,0 +1,15 @@
+_VERSION = 0.5
+VERSION  = `git describe --tags --dirty 2>/dev/null || echo $(_VERSION)`
+
+PKG_CONFIG = pkg-config
+
+# paths
+PREFIX = /usr/local
+MANDIR = $(PREFIX)/share/man
+DATADIR = $(PREFIX)/share
+
+#XWAYLAND =
+#XLIBS =
+# Uncomment to build XWayland support
+XWAYLAND = -DXWAYLAND
+XLIBS = xcb xcb-icccm
