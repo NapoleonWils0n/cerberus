490b963a84c702cdeaeda8d4011f73497f810732
Daniel J Wilcox <danieljwilcox@gmail.com>
2025-04-19 15:06:05 +0100

    nix-shell whisper

---
+++ nixos/nix-shell/whisper/shell.nix
@@ -0,0 +1,18 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+  buildInputs = [
+    (pkgs.python311Packages.openai-whisper.override {
+      triton = pkgs.python311Packages.triton-cuda;
+      torch = pkgs.python311Packages.pytorch-bin.override {
+        triton = pkgs.python311Packages.triton-cuda;
+      };
+    })
+    pkgs.cudaPackages_12_3.cudatoolkit
+  ];
+
+  shellHook = ''
+    echo "Welcome to the Whisper CUDA environment!"
+    echo "Run 'whisper input.mp3 --model small --device cuda' to transcribe."
+  '';
+}
